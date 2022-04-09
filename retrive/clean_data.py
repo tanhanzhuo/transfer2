7e02562afa57ad204ce31c9526f941da8cd572b5
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-09 21:35:35 +0800

    Update clean_data.py

--- retrive/clean_data.py
+++ retrive/clean_data.py
@@ -2,5 +2,5 @@ with open('data_extension.txt', 'r') as f:
     lines = f.readlines()
 with open('data_extension_clean.txt', 'w') as f:
     for line in lines:
-        tmp = line.replace('[USER]', '@USER').replace('[HTTP]', 'https://')
+        tmp = line.replace('[RT]', '').replace('[USER]', '@USER').replace('[HTTP]', 'https://')
         f.write(tmp)
