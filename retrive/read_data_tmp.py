aba3dca1873d6991b67267d5bea6636f9e1fff5c
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-09 21:41:21 +0800

    Create read_data_tmp.py

---
+++ retrive/read_data_tmp.py
@@ -0,0 +1,8 @@
+import datasets
+import random
+data = datasets.load_from_disk('./data_extension/TrainData_line')
+data_train = data['train']
+print(len(data_train))
+print(data_train[random.randint(len(data_train))])
+print(data_train[random.randint(len(data_train))])
+print(data_train[random.randint(len(data_train))])
