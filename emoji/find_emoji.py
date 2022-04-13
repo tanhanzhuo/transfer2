55cbd48d27322f455938e6b819eb83c9896e0c3d
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-13 23:01:47 +0800

    Update find_emoji.py

--- emoji/find_emoji.py
+++ emoji/find_emoji.py
@@ -11,9 +11,9 @@ with open('/work/data/twitter_ref.txt', 'r') as f:
             else:
                 emoji_dic[emoji_one] = 1
         if len(emoji_list) in emoji_num.keys():
-            emoji_num[emoji_list] += 1
+            emoji_num[len(emoji_list)] += 1
         else:
-            emoji_num[emoji_list] = 1
+            emoji_num[len(emoji_list)] = 1
 emoji_dic_sort = dict(sorted(emoji_dic.items(), key=lambda x: x[1]))
 emoji_num_sort = dict(sorted(emoji_num.items(), key=lambda x: x[0]))
 print(emoji_dic_sort)
