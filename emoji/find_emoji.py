9765e70011064cad8c1f2f3795acc9fa7a94dc49
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-13 23:00:44 +0800

    Update find_emoji.py

--- emoji/find_emoji.py
+++ emoji/find_emoji.py
@@ -1,6 +1,7 @@
 import emoji
 from tqdm import tqdm
 emoji_dic = {}
+emoji_num = {}
 with open('/work/data/twitter_ref.txt', 'r') as f:
     for line in tqdm(f):
         emoji_list = emoji.distinct_emoji_list(line)
@@ -9,5 +10,11 @@ with open('/work/data/twitter_ref.txt', 'r') as f:
                 emoji_dic[emoji_one] += 1
             else:
                 emoji_dic[emoji_one] = 1
-emoji_sort = dict(sorted(emoji_dic.items(), key=lambda x: x[1]))
-print(emoji_sort)
+        if len(emoji_list) in emoji_num.keys():
+            emoji_num[emoji_list] += 1
+        else:
+            emoji_num[emoji_list] = 1
+emoji_dic_sort = dict(sorted(emoji_dic.items(), key=lambda x: x[1]))
+emoji_num_sort = dict(sorted(emoji_num.items(), key=lambda x: x[0]))
+print(emoji_dic_sort)
+print(emoji_num_sort)
