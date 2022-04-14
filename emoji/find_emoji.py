ad29c3840556eb144f9a7c9d1969a13e5609eca8
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-14 22:30:54 +0800

    Update find_emoji.py

--- emoji/find_emoji.py
+++ emoji/find_emoji.py
@@ -18,15 +18,18 @@ emoji_num = {}
 # emoji_num_sort = dict(sorted(emoji_num.items(), key=lambda x: x[0]))
 # print(emoji_dic_sort)
 # print(emoji_num_sort)
-with open('/work/data/twitter_ref.txt', 'r') as f:
-    for line in tqdm(f):
-        emoji_list = emoji.distinct_emoji_list(line)
-        if len(emoji_list) == 1:
-            emoji_one = emoji_list[0]
-            if emoji_one in emoji_dic.keys():
-                emoji_dic[emoji_one] += 1
-            else:
-                emoji_dic[emoji_one] = 1
+with open('data_emoji.txt', 'w') as f_write:
+    with open('/work/data/twitter_ref.txt', 'r') as f_read:
+        for line in tqdm(f_read):
+            emoji_list = emoji.distinct_emoji_list(line)
+            if len(emoji_list) == 1:
+                emoji_one = emoji_list[0]
+                if emoji_one in emoji_dic.keys():
+                    emoji_dic[emoji_one] += 1
+                else:
+                    emoji_dic[emoji_one] = 1
+                line_clean = emoji.replace_emoji(line)
+                f_write.write(emoji_one + '\t' + line_clean)
 
 emoji_dic_sort = dict(sorted(emoji_dic.items(), key=lambda x: x[1]))
 print(emoji_dic_sort)
