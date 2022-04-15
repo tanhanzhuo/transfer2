c64ea4295d37b36bb3642315d532088ad8c2799f
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-15 20:37:11 +0800

    update

--- emoji/extract_emoji.py
+++ emoji/extract_emoji.py
@@ -1,8 +1,12 @@
 import random
+import json
 random.seed(0)
 # emoji_top = {'👀':0, '🙌':0, '😳':0, '💀':0, '😔':0, '😏':0, '😁':0, '❤':0, '🔥':0, '👌':0,
 #         '😒':0, '💯':0, '💕':0, '😘':0, '😊':0, '😩':0, '❤️':0, '😍':0, '😭':0, '😂':0}
 emoji_top = ['🤔', '🙄', '😳', '👌', '😁', '😏', '💯', '🔥', '💕', '😘', '😔', '❤', '♥', '😒', '😊', '😩', '❤️', '😍', '😭', '😂']
+label2idx = {}
+for idx in range(len(emoji_top)):
+    label2idx[emoji_top[idx]] = idx
 with open('data_emoji.txt', 'r') as f:
     data_emoji = f.readlines()
 
@@ -12,12 +16,19 @@ for data_one in data_emoji:
     line = data_one.split('\t')[1].strip().replace('  ',' ')
     if emoji_one in emoji_top:
         if len(line.split(' ')) > 5:
-            data_emoji_top.append(data_one.replace('https://','https'))
+            txt = line.replace('https://', 'https') + '\n'
+            lab = label2idx[emoji_one]
+            data_emoji_top.append(
+                {'label': lab, 'text': txt}
+            )
+
 random.shuffle(data_emoji_top)
 SP = int(len(data_emoji_top)*0.9)
-with open('data_emoji_train', 'w') as f:
+with open('train.json', 'w') as f:
     for idx in range(SP):
-        f.write(data_emoji_top[idx])
-with open('data_emoji_test', 'w') as f:
+        json.dump(data_emoji_top[idx], f)
+        f.write('\n')
+with open('dev.json', 'w') as f:
     for idx in range(SP,len(data_emoji_top)):
-        f.write(data_emoji_top[idx])
+        json.dump(data_emoji_top[idx], f)
+        f.write('\n')
