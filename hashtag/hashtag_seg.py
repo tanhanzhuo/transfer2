ad4dab92de008c8711407ee6aad2aece4e410539
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-20 21:33:50 +0800

    Update hashtag_seg.py

--- hashtag/hashtag_seg.py
+++ hashtag/hashtag_seg.py
@@ -1,17 +1,18 @@
 import os
 from tqdm import tqdm, trange
-import json
 data =[]
 with open('hash_his.json', 'r') as f:
     lines = f.readlines()
     for line in lines:
-        data.append(json.loads(line))
+        data.append(line.split('\t')[0])
+
 from hashformers import TransformerWordSegmenter as WordSegmenter
 ws = WordSegmenter(
     segmenter_model_name_or_path="gpt2",
     reranker_model_name_or_path="bert-base-uncased"
-)
-segmentations = ws.segment([
-    "#weneedanationalpark",
-    "#icecold"
-])
+).cuda()
+
+for one in tqdm(data):
+    segmentations = ws.segment([
+        one
+    ])
