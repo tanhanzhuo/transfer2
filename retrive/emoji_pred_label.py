0d0101c0914d83da028a26f3c5a62d17127ab8d1
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-20 17:50:09 +0800

    retrive_emoji

---
+++ retrive/emoji_pred_label.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+import random
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoConfig,AutoModelForSequenceClassification,DataCollatorWithPadding
+import datasets
+import torch
+from accelerate import Accelerator
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--output_dir", default='../retrive/data_extension/', type=str, required=False, help="The output directory where the model predictions and checkpoints will be written.")
+parser.add_argument("--dataset_path", default='../retrive/data_extension/TrainData_line', type=str, required=False, help="dataset name")
+parser.add_argument("--task_name", default='TrainData_line', type=str, required=False, help="dataset name")
+parser.add_argument("--use_slow_tokenizer", action="store_true", help="If passed, will use a slow tokenizer (not backed by the 🤗 Tokenizers library).")
+parser.add_argument("--model_name_or_path", default='../emoji/model/', type=str, required=False, help="tokenizer name")
+parser.add_argument("--max_seq_length", default=128, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
+parser.add_argument("--batch_size", default=32, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
+parser.add_argument("--preprocessing_num_workers", default=1, type=int, help="multi-processing number.")
+parser.add_argument("--overwrite_cache", type=bool, default=False, help="Overwrite the cached training and evaluation sets")
+
+# parser.add_argument("--model_name", default='/work/transfer2/emoji/model', type=str, required=False, help="tokenizer name")
+def convert_example(example, label2idx):
+    example.pop('special_tokens_mask')
+    example.pop('attention_mask')
+    example['label'] = label2idx[example['label']]
+    example['prob_map'] = [int(0) if i < 0.5 else int(1) for i in example['prob_map']]
+
+    return example  # ['input_ids'], example['token_type_ids'], label, prob
+
+@torch.no_grad()
+def pred_prob(args):
+    accelerator = Accelerator()
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, normalization=True)
+    config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=20)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, config=config)
+    model = accelerator.prepare(model)
+    model.eval()
+
+    for task in args.task_name.split(','):
+        tokenized_datasets = datasets.load_from_disk(args.dataset_path)
+        for SPLIT in ['train']:
+            train_dataset = tokenized_datasets[SPLIT]
+            train_dataset = train_dataset.remove_columns(['special_tokens_mask'])
+            batchify_fn = DataCollatorWithPadding(tokenizer=tokenizer)
+            train_data_loader = DataLoader(
+                train_dataset, shuffle=False, collate_fn=batchify_fn, batch_size=args.batch_size
+            )
+            train_data_loader = accelerator.prepare(train_data_loader)
+            emoji_pred = []
+            for step, batch in enumerate(train_data_loader):
+                # batch.pop('special_tokens_mask')
+                outputs = model(**batch)
+                preds = outputs.logits.argmax(dim=-1).cpu().numpy()
+                emoji_pred.extend(preds)
+            train_dataset = train_dataset.add_column("labels", emoji_pred)
+            tokenized_datasets[SPLIT] = train_dataset
+        tokenized_datasets.save_to_disk(args.output_dir + '/emoji')
+        print('task done! {}'.format(task))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    pred_prob(args)
---
+++ retrive/emoji_pred_train.py
@@ -0,0 +1,232 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import logging
+import os
+import sys
+import random
+import time
+import math
+import distutils.util
+from functools import partial
+import copy
+import numpy as np
+import datasets
+# import paddle
+# from paddle.io import DataLoader
+
+import torch
+from torch.utils.data import DataLoader
+import transformers
+from transformers import (
+    AdamW,
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    PretrainedConfig,
+    SchedulerType,
+    default_data_collator,
+    get_scheduler,
+    set_seed,
+)
+from accelerate import Accelerator
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, classification_report
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument(
+        "--model_name_or_path",
+        default='vinai/bertweet-base',
+        type=str,
+        required=False,
+        help="Path to pre-trained model or shortcut name selected in the list: "
+    )
+    parser.add_argument(
+        "--token_name_or_path",
+        default='vinai/bertweet-base',
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "--input_dir",
+        default='./data_extension/emoji/',
+        type=str,
+        required=False,
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        default='./model_emoji/',
+        type=str,
+        required=False,
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+    parser.add_argument(
+        "--results_name",
+        default='results_all.txt',
+        type=str,
+        required=False,
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+    parser.add_argument(
+        "--max_seq_length",
+        default=128,
+        type=int,
+        help="The maximum total input sequence length after tokenization. Sequences longer "
+             "than this will be truncated, sequences shorter will be padded.", )
+    parser.add_argument(
+        "--learning_rate",
+        default='1e-5',
+        type=str,
+        help="The initial learning rate for Adam.")
+    parser.add_argument(
+        "--num_train_epochs",
+        default=20,
+        type=int,
+        help="Total number of training epochs to perform.", )
+    parser.add_argument(
+        "--logging_steps",
+        type=int,
+        default=1000,
+        help="Log every X updates steps.")
+    parser.add_argument(
+        "--save_steps",
+        type=int,
+        default=10000,
+        help="Save checkpoint every X updates steps.")
+    parser.add_argument(
+        "--batch_size",
+        default=32,
+        type=int,
+        help="Batch size per GPU/CPU for training.", )
+    parser.add_argument(
+        "--weight_decay",
+        default=0.0,
+        type=float,
+        help="Weight decay if we apply some.")
+    parser.add_argument(
+        "--lr_scheduler_type",
+        type=SchedulerType,
+        default="linear",
+        help="The scheduler type to use.",
+        choices=["linear", "cosine", "cosine_with_restarts", "polynomial", "constant", "constant_with_warmup"],
+    )
+    parser.add_argument(
+        "--num_warmup_steps", type=int, default=0, help="Number of steps for the warmup in the lr scheduler."
+    )
+    parser.add_argument(
+        "--max_train_steps",
+        default=None,
+        type=int,
+        help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
+    )
+    parser.add_argument(
+        "--seed", default=42, type=int, help="random seed for initialization")
+    parser.add_argument(
+        "--device",
+        default="0",
+        type=str,
+        help="The device to select to train the model, is must be cpu/gpu/xpu.")
+
+    args = parser.parse_args()
+    return args
+
+def do_train(args):
+    # set_seed(args.seed)
+    accelerator = Accelerator()
+    data_all = datasets.load_from_disk(args.input_dir)
+    emoji_top = ['🤔', '🙄', '😳', '👌', '😁', '😏', '💯', '🔥', '💕', '😘', '😔', '❤', '♥', '😒', '😊', '😩', '❤️',
+                 '😍', '😭', '😂']
+    label2idx = {}
+    for idx in range(len(emoji_top)):
+        label2idx[idx] = emoji_top[idx]
+    train_ds = data_all['train']
+
+    num_classes = len(label2idx.keys())
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, normalization=True)
+    config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=num_classes)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path, config=config)
+
+    batchify_fn = DataCollatorWithPadding(tokenizer=tokenizer)
+    train_data_loader = DataLoader(
+        train_ds, shuffle=True, collate_fn=batchify_fn, batch_size=args.batch_size
+    )
+    dev_data_loader = DataLoader(
+        dev_ds, shuffle=True, collate_fn=batchify_fn, batch_size=args.batch_size
+    )
+
+    print('data ready!!!')
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": args.weight_decay,
+        },
+        {
+            "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+            "weight_decay": 0.0,
+        },
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=float(args.learning_rate))
+    num_update_steps_per_epoch = len(train_data_loader)
+    args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
+    lr_scheduler = get_scheduler(
+        name=args.lr_scheduler_type,
+        optimizer=optimizer,
+        num_warmup_steps=args.num_warmup_steps,
+        num_training_steps=args.max_train_steps,
+    )
+
+    model, optimizer, train_data_loader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_data_loader, lr_scheduler
+    )
+
+    print('start Training!!!')
+    global_step = 0
+    tic_train = time.time()
+    best_metric = [0, 0, 0]
+    for epoch in range(args.num_train_epochs):
+        model.train()
+        for step, batch in enumerate(train_data_loader):
+            global_step += 1
+            # batch.pop('special_tokens_mask')
+            output = model(**batch)
+            loss = output.loss
+            accelerator.backward(loss)
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            if (global_step + 1) % args.logging_steps == 0:
+                print(
+                    "global step %d/%d, epoch: %d, loss: %f, speed: %.4f step/s"
+                    % (global_step, args.max_train_steps, epoch,
+                       loss.item(), args.logging_steps / (time.time() - tic_train)))
+                tic_train = time.time()
+
+        accelerator.wait_for_everyone()
+        unwrapped_model = accelerator.unwrap_model(model)
+        unwrapped_model.save_pretrained(args.output_dir, save_function=accelerator.save)
+        tokenizer.save_pretrained(args.output_dir + str(epoch))
+        del unwrapped_model
+        torch.cuda.empty_cache()
+    del model#, optimizer, logits, logits_seq, loss, loss_seq, loss_all, accelerator
+    torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    do_train(args)
