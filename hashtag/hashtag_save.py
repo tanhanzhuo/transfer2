35b55ec1180eae433237e15178012e37f9bea7a0
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-21 21:51:57 +0800

    1

--- hashtag/hashtag_save.py
+++ hashtag/hashtag_save.py
@@ -13,6 +13,8 @@ with open(filePath, 'r') as f:
         hash_tmp = HASH.findall(line)
         for hash_one in hash_tmp:
             hash_one = hash_one.lower()
+            if len(hash_one) > 30:
+                continue
             if hash_one[1].isalpha():
                 if hash_one[-1] == '…':
                     continue
--- hashtag/hashtag_seg.py
+++ hashtag/hashtag_seg.py
@@ -3,42 +3,24 @@ import string
 import torch
 from tqdm import tqdm, trange
 import re
-data ={}
+data =[]
 num = []
 with open('hash_his.txt', 'r',encoding='utf-8') as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
-        hash = line.split('\t')[0].lower()
-        if hash[-1] == '…':
-            continue
-        if len(hash) > 3 and  hash[-3:] == '...':
-            continue
-        if hash[-1] in string.punctuation:
-            hash = hash[:-1]
-        hash_clean = re.findall('[a-z0-9]*', hash)
-        hash_clean = '#'+''.join(hash_clean)
-        if hash == hash_clean:
-            if hash in data.keys():
-                data[hash] += int(line.split('\t')[1])
-            else:
-                data[hash] = int(line.split('\t')[1])
-
-
-data_hash = list(data.keys())
+        data.append(line.split('\t')[0])
+        num.append(line.split('\t')[1])
 
 from hashformers import TransformerWordSegmenter as WordSegmenter
 with torch.no_grad():
     data_seg = []
-    ws = WordSegmenter(
-        segmenter_model_name_or_path="distilgpt2"
-        # reranker_model_name_or_path="bert-base-uncased"
-    )
+    ws = WordSegmenter( segmenter_model_name_or_path="distilgpt2")
 
     with open('hash_seg.txt', 'a') as f:
-        for idx in trange(int(len(data_hash)/2)):
-            segmentations = ws.segment([data_hash[idx]])
-            f.write(data_hash[idx] + '\t' + segmentations[0] + '\t' + str(data[data_hash[idx]]) + '\n')
+        for idx in trange(int(len(data)/2)):
+            segmentations = ws.segment([data[idx]])
+            f.write(data[idx] + '\t' + segmentations[0] + '\t' + num[idx] + '\n')
             if (idx+1) % 10000 ==0:
                 del ws,segmentations
                 torch.cuda.empty_cache()
