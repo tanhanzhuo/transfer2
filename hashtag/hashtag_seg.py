25d8af06750f71eecb46ab99036d8f69fe7824d9
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-21 22:08:41 +0800

    Update hashtag_seg.py

--- hashtag/hashtag_seg.py
+++ hashtag/hashtag_seg.py
@@ -5,7 +5,7 @@ from tqdm import tqdm, trange
 import re
 data =[]
 num = []
-with open('hash_his.txt', 'r',encoding='utf-8') as f:
+with open('hash_his.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip()
@@ -15,13 +15,9 @@ with open('hash_his.txt', 'r',encoding='utf-8') as f:
 from hashformers import TransformerWordSegmenter as WordSegmenter
 with torch.no_grad():
     data_seg = []
-    ws = WordSegmenter( segmenter_model_name_or_path="distilgpt2")
+    ws = WordSegmenter( segmenter_model_name_or_path="gpt2")
 
     with open('hash_seg.txt', 'a') as f:
         for idx in trange(int(len(data)/2)):
             segmentations = ws.segment([data[idx]])
             f.write(data[idx] + '\t' + segmentations[0] + '\t' + num[idx] + '\n')
-            if (idx+1) % 10000 ==0:
-                del ws,segmentations
-                torch.cuda.empty_cache()
-                ws = WordSegmenter(segmenter_model_name_or_path="distilgpt2")
