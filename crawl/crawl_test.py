de8ffdd3245813c3ee038fdd7a790f89fd54156e
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-04-25 21:01:01 +0800

    Update crawl_test.py

--- crawl/crawl_test.py
+++ crawl/crawl_test.py
@@ -1,16 +1,55 @@
-import tweepy
-consumer_key="b0qF9CcEslZWdfTY6Gyfr6k7i"
-consumer_secret="x8tKpNAUbeiCaTCFyIBmDvpXHsCD91ZOx0MHq4rU7ZN92yZI4k"
-access_token="1517497475749539841-9LVF5opQbfkuUnreMz5v3PsyxZCID1"
-access_token_secret="Ok2vxvbvJWS579a3fAgjkytr5iVoZMWwVUGMLr5FkhJcQ"
+import requests
+import os
+import json
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = os.environ.get("BEARER_TOKEN")
 
-api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print('Successful Authentication')
-except:
-    print('Failed authentication')
+def create_url():
+    tweet_fields = "tweet.fields=lang,author_id"
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    ids = "ids=1278747501642657792,1255542774432063488"
+    # You can adjust ids to include a single Tweets.
+    # Or you can add to up to 100 comma-separated IDs
+    url = "https://api.twitter.com/2/tweets?{}&{}".format(ids, tweet_fields)
+    return url
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2TweetLookupPython"
+    return r
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def main():
+    url = create_url()
+    json_response = connect_to_endpoint(url)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
