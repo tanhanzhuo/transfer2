dc193cf3820e4259bad963c602d166d854a6c806
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-05-13 11:09:15 +0800

    Update emoji_distill.py

--- emoji_distill/emoji_distill.py
+++ emoji_distill/emoji_distill.py
@@ -396,10 +396,10 @@ def do_train(args):
                 optimizer.zero_grad()
             if (epoch + 1) % args.logging_steps == 0:
                 print(
-                    "global step %d/%d, epoch: %d, loss: %f, speed: %.4f step/s, seed: %d,lr: %.5f,task: %s"
+                    "global step %d/%d, epoch: %d, loss: %f,loss_emoji: %f, speed: %.4f step/s, seed: %d,lr: %.5f,task: %s"
                     % (global_step, args.max_train_steps, epoch,
-                       loss_all, args.logging_steps / (time.time() - tic_train),
-                       args.seed,float(lr),args.input_dir))
+                       loss, loss_emoji, args.logging_steps / (time.time() - tic_train),
+                       args.seed, float(lr), args.input_dir))
                 tic_train = time.time()
             if (epoch + 1) % args.save_steps == 0:
                 tic_eval = time.time()
@@ -434,6 +434,7 @@ def do_train(args):
     print("f1macro:%.5f, acc:%.5f, acc: %.5f, " % (best_metric[0], best_metric[1], best_metric[2]))
     print("f1macro:%.5f, acc:%.5f, acc: %.5f " % (cur_metric[0], cur_metric[1], cur_metric[2]))
     del model
+    torch.cuda.empty_cache()
     return cur_metric
 
 if __name__ == "__main__":
