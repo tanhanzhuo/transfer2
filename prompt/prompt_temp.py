70aff9f944bd9b27b24fb0426d279b9d7c3c9ea4
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-11 23:42:59 +0800

    Update prompt_temp.py

--- prompt/prompt_temp.py
+++ prompt/prompt_temp.py
@@ -281,7 +281,7 @@ def do_train(args):
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters1 = [
         {'params': [p for n, p in prompt_model.plm.named_parameters() if not any(nd in n for nd in no_decay)],
-         'weight_decay': 0.00},
+         'weight_decay': 0.01},
         {'params': [p for n, p in prompt_model.plm.named_parameters() if any(nd in n for nd in no_decay)],
          'weight_decay': 0.0}
     ]
