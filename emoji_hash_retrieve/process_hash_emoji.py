d31d902379e50321139d278c4e69b9fab54e3ed3
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-11 16:55:51 +0800

    Update process_hash_emoji.py

--- emoji_hash_retrieve/process_hash_emoji.py
+++ emoji_hash_retrieve/process_hash_emoji.py
@@ -2,49 +2,71 @@ import json
 import re
 import emoji
 import numpy as np
+import random
 from transformers import AutoTokenizer
+
+token = AutoTokenizer.from_pretrained('vinai/bertweet-base', normalization=True)
+HASH = re.compile(r"#\S+")
+MIN = 2
 def write_json(fileName,data):
     with open(fileName, 'w', encoding='utf-8') as f:
         for one in data:
             json.dump(one, f)
             f.write('\n')
-token = AutoTokenizer.from_pretrained('vinai/bertweet-base', normalization=True)
-KTH = 2
-HASH = re.compile(r"#\S+")
-for task in 'sem-18,sem19-task6-offen'.split(','):
-    for sp in ['train','dev','test']:
-        data = []
-        with open('../finetune/data/'+task+'/'+sp+'_emo_hash_retrieve.json') as f:
-            for line in f:
-                one  = json.loads(line)
-                hash_one = one['hash']
-                emoji_one = one['emoji']
-                similarity = []
-                retrieve_all = one.pop('retrieve')
-                if len(retrieve_all) < KTH:
-                    continue
-                for retrieve in retrieve_all:
-                    hash_re = HASH.findall(retrieve)
-                    emoji_re = emoji.distinct_emoji_list(retrieve)
-                    hash_same = list(set(hash_re) & set(hash_one))
-                    emoji_same = list(set(emoji_re) & set(emoji_one))
-                    similarity.append(len(hash_same)+len(emoji_same))
-                best = np.argpartition(np.array(similarity), -KTH)[-KTH:]
 
-                for idx in best:
-                    text = retrieve_all[idx].replace('[RT]', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip()
-                    if len(token(text)['input_ids']) > 30:
-                        emoji_re = emoji.distinct_emoji_list(text)
-                        emoji_same = list(set(emoji_re) & set(emoji_one))
-                        text = emoji.replace_emoji(text) + ''.join(emoji_same)
-
-                        hash_re = HASH.findall(text)
-                        hash_same = list(set(hash_re) & set(hash_one))
-                        for hash_tmp in hash_re:
-                            if hash_tmp not in hash_same:
-                                text = text.replace(hash_tmp+' ', '')
-                        print(len(token(text)['input_ids']))
-                        
-                    one['text'] = text+ ' '+token.eos_token + ' '+ one['text']
+def read_data(task,sp,KTH):
+    data = []
+    with open('../finetune/data/' + task + '/' + sp + '_emo_hash_retrieve.json') as f:
+        for line in f:
+            one = json.loads(line)
+            hash_one = one['hash']
+            emoji_one = one['emoji']
+            similarity = []
+            retrieve_all = one.pop('retrieve')
+            if len(retrieve_all) < MIN:
+                continue
+            if KTH == 0:
                 data.append(one)
-        write_json('../finetune/data/'+task+'/'+sp+'_emo_hash_process.json', data)
+                continue
+
+            for retrieve in retrieve_all:
+                hash_re = HASH.findall(retrieve)
+                emoji_re = emoji.distinct_emoji_list(retrieve)
+                hash_same = list(set(hash_re) & set(hash_one))
+                emoji_same = list(set(emoji_re) & set(emoji_one))
+                similarity.append(len(hash_same) + len(emoji_same))
+            best = np.argpartition(np.array(similarity), -KTH)[-KTH:]
+
+            for idx in best:
+                text = retrieve_all[idx].replace('[RT]', '').replace('[USER]', '@USER').replace('[HTTP]',
+                                                                                                'https').strip()
+                if len(token(text)['input_ids']) > 30:
+                    emoji_re = emoji.distinct_emoji_list(text)
+                    emoji_same = list(set(emoji_re) & set(emoji_one))
+                    text = emoji.replace_emoji(text) + ''.join(emoji_same)
+
+                    hash_re = HASH.findall(text)
+                    hash_same = list(set(hash_re) & set(hash_one))
+                    for hash_tmp in hash_re:
+                        if hash_tmp not in hash_same:
+                            text = text.replace(hash_tmp + ' ', '')
+                    print(len(token(text)['input_ids']))
+
+                one['text'] = text + ' ' + token.eos_token + ' ' + one['text']
+            data.append(one)
+    return data
+
+for task in 'sem-18,sem19-task6-offen'.split(','):
+    for KTH in [0,1,2]:
+        data_train = []
+        for sp in ['train','dev']:
+            data_train.extend(read_data(task,sp,KTH))
+        if KTH == 0:
+            idx_perm = list(range(len(data_train)))
+            random.shuffle(idx_perm)
+        NUM = int(len(data_train)*0.1)
+        write_json('../finetune/data/'+task+'/'+'train'+'_emo_hash_process'+str(KTH)+'.json', data_train[idx_perm[:NUM*9]])
+        write_json('../finetune/data/' + task + '/' + 'dev' + '_emo_hash_process'+str(KTH)+'.json', data_train[idx_perm[NUM * 9:]])
+        data_test = read_data(task,'test')
+        write_json('../finetune/data/' + task + '/' + 'test' + '_emo_hash_process'+str(KTH)+'.json', data_test)
+
