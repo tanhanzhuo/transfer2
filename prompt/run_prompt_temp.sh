b3ea9bee7682e3bfbb9084b1f77b5060110e7358
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-13 09:48:37 +0800

    Update run_prompt_temp.sh

--- prompt/run_prompt_temp.sh
+++ prompt/run_prompt_temp.sh
@@ -4,7 +4,7 @@ do
   do
     for KTH in 0 1 2
     do
-      CUDA_VISIBLE_DEVICES=7 python prompt_temp.py --task_name sem-18,sem19-task6-offen --method "_emo_hash_process_seed${SEED}_${ORDER}_kth${KTH}" --results_name "results_emo_hash_seed${SEED}_${ORDER}_kth${KTH}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot 16 --train_dev 8
+      CUDA_VISIBLE_DEVICES=7 python prompt_temp.py --task_name sem-18,sem19-task6-offen --method "_emo_hash_process_seed${SEED}_${ORDER}_kth${KTH}" --results_name "results_emo_hash_seed${SEED}_${ORDER}_kth${KTH}_shot16.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot 16 --train_dev 8
     done
   done
 done
