620acc949b68cb8ec50a6703747393d83fc98297
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-13 11:08:49 +0800

    Update run_finetune.sh

--- emoji_hash_retrieve/run_finetune.sh
+++ emoji_hash_retrieve/run_finetune.sh
@@ -1,10 +1,10 @@
-for SEED in 0 1 2
+for SEED in 0 1 2 3 4
 do
   for ORDER in refirst orifirst
   do
     for KTH in 0 1 2
     do
-      CUDA_VISIBLE_DEVICES=7 python finetune_lrtune_macrof1_fromtxt.py --task_name sem-18,sem19-task6-offen --method "_emo_hash_process_seed${SEED}_${ORDER}_kth${KTH}" --results_name "results_emo_hash_seed${SEED}_${ORDER}_kth${KTH}.txt" --learning_rate 3e-5 --num_train_epochs 15
+      CUDA_VISIBLE_DEVICES=7 python finetune_lrtune_macrof1_fromtxt.py --task_name sem-18,sem19-task6-offen --method "_emo_hash_process_seed${SEED}_${ORDER}_kth${KTH}" --results_name "results_emo_hash_seed${SEED}_${ORDER}_kth${KTH}.txt" --learning_rate 1e-5 --num_train_epochs 15
     done
   done
 done
