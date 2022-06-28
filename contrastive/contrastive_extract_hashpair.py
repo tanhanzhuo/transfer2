443b9a9961dd992d44489ce4010949cc183d2f6a
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-28 16:49:13 +0800

    hash pair

--- contrastive/contrastive_extract_hashpair.py
+++ contrastive/contrastive_extract_hashpair.py
@@ -55,16 +55,17 @@ hash_data = {}
 for hash_one in hash_thre_list:
     hash_data[hash_one] = set()
 with open(filePath, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    for idx in trange(len(lines)):
-        line = lines[idx]
-    # for line in tqdm(f):
+    # lines = f.readlines()
+    # for idx in trange(len(lines)):
+    #     line = lines[idx]
+    for line in tqdm(f):
         hash_tmp_clean = process(line)
         for hash_one in hash_tmp_clean:
             tmp = hash_data.get(hash_one)
 
             if tmp is not None:
-                hash_data[hash_one].add(idx)
+                # hash_data[hash_one].add(idx)
+                hash_data[hash_one].add(line.replace('[RT] ', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip())
 
 NUM = args.num
 hash_pair = []
@@ -74,6 +75,7 @@ for hash_one in tqdm(hash_thre_list):
         continue
     for tmp in range(NUM):
         data_tmp = random.sample(data, 2)
-        hash_pair.append(  {'text1':lines[data_tmp[0]].replace('[RT] ', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip(), \
-                            'text2':lines[data_tmp[1]].replace('[RT] ', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip()}  )
+        # hash_pair.append(  {'text1':lines[data_tmp[0]].replace('[RT] ', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip(), \
+        #                     'text2':lines[data_tmp[1]].replace('[RT] ', '').replace('[USER]', '@USER').replace('[HTTP]', 'https').strip()}  )
+        hash_pair.append({'text1': data_tmp[0],'text2': data_tmp[1]})
 write_json('hash_pair_thre'+str(args.thre)+'_num'+str(args.num), hash_pair)
