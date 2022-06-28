38c313c3afca07e34ebbb6284ca91eb03a5dde66
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-06-28 18:22:26 +0800

    Create hashtag_seg.py

---
+++ contrastive/hashtag_seg.py
@@ -0,0 +1,24 @@
+import os
+import string
+import torch
+from tqdm import tqdm, trange
+import json
+import re
+data =[]
+num = []
+f=open('./contrastive/hash_his.json','r',encoding='utf-8')
+hash_dic = json.load(f)
+f.close()
+for hash_one in list(hash_dic.keys()):
+    if hash_dic[hash_one] < 100:
+        hash_dic.pop(hash_one)
+
+from hashformers import TransformerWordSegmenter as WordSegmenter
+with torch.no_grad():
+    data_seg = []
+    ws = WordSegmenter( segmenter_model_name_or_path="gpt2")
+
+    with open('hash_seg.txt', 'a') as f:
+        for hash_one in tqdm(hash_dic.keys()):
+            segmentations = ws.segment([hash_one])
+            f.write(hash_one + '\t' + segmentations[0] + '\t' + hash_dic[hash_one] + '\n')
