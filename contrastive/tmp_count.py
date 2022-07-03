3a9ffa6d0ae297592a60584b57c7de66cfeeb64d
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-03 21:02:29 +0800

    59999

---
+++ contrastive/tmp_count.py
@@ -0,0 +1,28 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_path", default='../finetune/data/', type=str, required=False, help="dataset name")
+parser.add_argument("--task_name", default='stance,hate,sem-18,sem-17,imp-hate,sem19-task5-hate,sem19-task6-offen,sem22-task6-sarcasm', type=str, required=False, help="dataset name")
+args = parser.parse_args()
+
+def read_data(fileName):
+    with open(fileName, 'r', encoding='utf-8') as f:
+        data = []
+        lines = f.readlines()
+        for line in lines:
+            data.append({'labels': line.split('\t')[0], 'text': line.split('\t')[1]})
+    return data
+
+TT=0
+for task in args.task_name.split(','):
+    total_num = 0
+    for fileName in ['train', 'dev', 'test']:
+        train_dataset = read_data(args.dataset_path + task + '/' + fileName)
+        total_num += len(train_dataset)
+    TT+=total_num
+    print("task:{}, num:{}".format(task,total_num))
+print(TT)
+
+stance,hate,sem-18,sem22-task6-sarcasm
+sem-17
+imp-hate
+sem19-task5-hate,sem19-task6-offen
