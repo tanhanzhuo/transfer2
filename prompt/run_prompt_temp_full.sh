c26f6899de9ea4e82360e07d7c5664cffe45d026
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-03 16:27:39 +0800

    shots rename

---
+++ prompt/run_prompt_temp_full.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=2 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK.txt" --template "It was {'mask'}" --num_train_epochs 15
+CUDA_VISIBLE_DEVICES=2 python prompt_temp.py --method _modelT100N100S_fileT100S_0 --results_name "results_lr3e5_epoch15_tempITWASMASK_modelT100N100S_fileT100S_K1.txt" --template "It was {'mask'}" --num_train_epochs 15
+CUDA_VISIBLE_DEVICES=2 python prompt_temp.py --method _modelT100N100S_fileT100S_1 --results_name "results_lr3e5_epoch15_tempITWASMASK_modelT100N100S_fileT100S_K2.txt" --template "It was {'mask'}" --num_train_epochs 15
