be4cfedbc0180567b192ffd69126c20d418b2a35
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-03 17:08:16 +0800

    Update run_prompt_temp_shots.sh

--- prompt/run_prompt_temp_shots.sh
+++ prompt/run_prompt_temp_shots.sh
@@ -1,5 +1,9 @@
 for SHOT in 16 32 64 128 256 512 1024
 do
+    CUDA_VISIBLE_DEVICES=1 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
+done
+for SHOT in 16 32 64 128 256 512 1024
+do
   for KTH in 0 1
   do
     CUDA_VISIBLE_DEVICES=1 python prompt_temp.py --method "_modelT100N100S_fileT100S_${KTH}" --results_name "results_modelT100N100S_fileT100S_${KTH}_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
