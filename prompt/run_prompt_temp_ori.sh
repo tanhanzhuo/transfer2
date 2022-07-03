bd0710254c59fed543773a08a7dc148179cacb61
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-03 20:41:47 +0800

    Update run_prompt_temp_ori.sh

--- prompt/run_prompt_temp_ori.sh
+++ prompt/run_prompt_temp_ori.sh
@@ -1,5 +1,5 @@
 for SHOT in 16 32 64 128 256 512 1024
 do
-    CUDA_VISIBLE_DEVICES=0 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
+    CUDA_VISIBLE_DEVICES=5 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
 done
-CUDA_VISIBLE_DEVICES=0 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK.txt" --template "It was {'mask'}" --num_train_epochs 15
+CUDA_VISIBLE_DEVICES=5 python prompt_temp.py --results_name "results_lr3e5_epoch15_tempITWASMASK.txt" --template "It was {'mask'}" --num_train_epochs 15
