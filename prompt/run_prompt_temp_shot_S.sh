58f9bc5a930a1033b78f5dc5adb7d6c7ac29336e
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-06 11:10:10 +0800

    ss remove test # to match

--- prompt/run_prompt_temp_shot_S.sh
+++ prompt/run_prompt_temp_shot_S.sh
@@ -2,6 +2,6 @@ for SHOT in 16 32 64 128 256 512 1024
 do
   for KTH in 0
   do
-    CUDA_VISIBLE_DEVICES=2 python prompt_temp.py --method "_modelT100N100S_fileT100S_${KTH}" --results_name "results_modelT100N100S_fileT100S_${KTH}_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
+    CUDA_VISIBLE_DEVICES=2 python prompt_temp.py --method "_modelT100N100S_fileT100SS_${KTH}" --results_name "results_modelT100N100S_fileT100SS_${KTH}_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
   done
 done
