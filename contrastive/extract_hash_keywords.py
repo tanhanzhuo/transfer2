e24ed77dcfff9c2274f789352ba8abdec8a80f4c
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-08 16:52:11 +0800

    Update extract_hash_keywords.py

--- contrastive/extract_hash_keywords.py
+++ contrastive/extract_hash_keywords.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--model',default='nltk',type=int)
+parser.add_argument('--model',default='nltk',type=str)
 args = parser.parse_args()
 
 import json
