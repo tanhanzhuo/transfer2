a841180cf805079d7fe3a2ecfc928fd13cea940d
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-11 09:49:38 +0800

    _

--- prompt/run_prompt_temp_shot.sh
+++ prompt/run_prompt_temp_shot.sh
@@ -2,6 +2,6 @@ for SHOT in 16 32 64 128 256 512 1024
 do
   for KTH in 0
   do
-    CUDA_VISIBLE_DEVICES=0 python prompt_temp.py --method "modelT100N100S_fileT100S_top${KTH}_hashfirst" --results_name "modelT100N100S_fileT100S_top${KTH}_hashfirst_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
+    CUDA_VISIBLE_DEVICES=0 python prompt_temp.py --method "_modelT100N100S_fileT100S_top${KTH}_hashfirst" --results_name "modelT100N100S_fileT100S_top${KTH}_hashfirst_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
   done
 done
--- prompt/run_prompt_temp_shot_S1.sh
+++ prompt/run_prompt_temp_shot_S1.sh
@@ -2,6 +2,6 @@ for SHOT in 16 32 64 128 256 512 1024
 do
   for KTH in 1
   do
-    CUDA_VISIBLE_DEVICES=3 python prompt_temp.py --method "modelT100N100S_fileT100S_top${KTH}_hashlast" --results_name "modelT100N100S_fileT100S_top${KTH}_hashlast_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
+    CUDA_VISIBLE_DEVICES=3 python prompt_temp.py --method "_modelT100N100S_fileT100S_top${KTH}_hashlast" --results_name "modelT100N100S_fileT100S_top${KTH}_hashlast_shot${SHOT}.txt" --template "It was {'mask'}" --num_train_epochs 15 --shot ${SHOT}
   done
 done
