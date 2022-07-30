dd635d363e149dbe1f3c25125a2e062ae10eacab
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-30 17:38:27 +0800

    Update shuffle_pair.py

--- contrastive_emoji/shuffle_pair.py
+++ contrastive_emoji/shuffle_pair.py
@@ -5,9 +5,10 @@ parser.add_argument('--num',default=1000,type=int)
 args = parser.parse_args()
 
 import json
+from tqdm import tqdm
 data = []
 with open('emoji_pair_thre'+str(args.thre)+'_num'+str(args.num), 'r', encoding='utf-8') as f:
-    for line in f:
+    for line in tqdm(f):
         one = json.loads(line)
         data.append(one)
 
