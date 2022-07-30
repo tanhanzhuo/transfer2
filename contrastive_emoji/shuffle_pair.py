7815bf1d30db1cb6dbba4650f6858c9f2489d611
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-07-30 17:37:44 +0800

    Create shuffle_pair.py

---
+++ contrastive_emoji/shuffle_pair.py
@@ -0,0 +1,23 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--thre',default=100000,type=int)
+parser.add_argument('--num',default=1000,type=int)
+args = parser.parse_args()
+
+import json
+data = []
+with open('emoji_pair_thre'+str(args.thre)+'_num'+str(args.num), 'r', encoding='utf-8') as f:
+    for line in f:
+        one = json.loads(line)
+        data.append(one)
+
+import random
+random.shuffle(data)
+
+def write_json(fileName,data):
+    with open(fileName + '.json', 'w', encoding='utf-8') as f:
+        for one in data:
+            tmp = json.dumps(one, ensure_ascii=False)
+            f.write(tmp+'\n')
+
+write_json('emoji_pair_thre'+str(args.thre)+'_num'+str(args.num)+'_shuffle', data)
