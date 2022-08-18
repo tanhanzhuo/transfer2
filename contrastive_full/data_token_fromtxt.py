726d1c867c1af10d5884f9d14d669ed8a29e7f04
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-08-18 11:32:46 +0800

    use simcse

--- contrastive_full/data_token_fromtxt.py
+++ contrastive_full/data_token_fromtxt.py
@@ -12,7 +12,7 @@ parser.add_argument("--output_dir", default='./', type=str, required=False, help
 parser.add_argument("--dataset_path", default='/work/data/', type=str, required=False, help="dataset name")
 parser.add_argument("--task_name", default='twitter_hash', type=str, required=False, help="dataset name")
 parser.add_argument("--use_slow_tokenizer", action="store_true", help="If passed, will use a slow tokenizer (not backed by the 🤗 Tokenizers library).")
-parser.add_argument("--tokenizer_name", default='vinai/bertweet-base', type=str, required=False, help="tokenizer name")
+parser.add_argument("--tokenizer_name", default='princeton-nlp/sup-simcse-roberta-base', type=str, required=False, help="tokenizer name")
 parser.add_argument("--max_seq_length", default=128, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
 parser.add_argument("--preprocessing_num_workers", default=10, type=int, help="multi-processing number.")
 parser.add_argument("--overwrite_cache", type=bool, default=False, help="Overwrite the cached training and evaluation sets")
@@ -48,10 +48,12 @@ def tokenization(args):
     raw_datasets = datasets.load_dataset('text', data_files=data_files)
     raw_datasets["train"] = raw_datasets["train"].shuffle()
     # Load pretrained tokenizer
-    if args.tokenizer_name:
+    if 'bertweet' in args.tokenizer_name:
         tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name, use_fast=not args.use_slow_tokenizer,
                                                   normalization=True)
-
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name, use_fast=not args.use_slow_tokenizer)
+        
     column_names = raw_datasets["train"].column_names
     text_column_name = "text" if "text" in column_names else column_names[0]
     padding = False
