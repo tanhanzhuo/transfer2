a6248ef15e89608bb9508aa46b0cb212e0896a95
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-08-20 20:05:22 +0800

    updates

--- contrastive_full/extract_feature_simcse.py
+++ contrastive_full/extract_feature_simcse.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 import os
 import numpy as np
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset_path", default='twitter_hash_thre100_num1000_test', type=str, required=False, help="dataset name")
+parser.add_argument("--dataset_path", default='twitter_hash_join_thre100_num1000_test', type=str, required=False, help="dataset name")
 parser.add_argument("--model_name", default='princeton-nlp/sup-simcse-roberta-base', type=str, required=False, help="tokenizer name")
 parser.add_argument("--max_seq_length", default=128, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
 parser.add_argument("--batch_size", default=64, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
 parser.add_argument("--save", default='./features/', type=str, required=False, help="dataset name")
-parser.add_argument("--split", default=200, type=int, required=False, help="dataset name")
+parser.add_argument("--split", default=100, type=int, required=False, help="dataset name")
 
 args = parser.parse_args()
 
@@ -20,6 +20,7 @@ from dataclasses import dataclass, field
 from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
 from transformers.tokenization_utils_base import BatchEncoding, PaddingStrategy, PreTrainedTokenizerBase
 
+@dataclass
 class MyDataCollatorWithPadding:
     tokenizer: PreTrainedTokenizerBase
     padding: Union[bool, str, PaddingStrategy] = True
@@ -28,7 +29,9 @@ class MyDataCollatorWithPadding:
     return_tensors: str = "pt"
 
     def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
-        text = features.pop('text')
+        text = []
+        for one in features:
+            text.append(one.pop('text'))
         batch = self.tokenizer.pad(
             features,
             padding=self.padding,
@@ -52,7 +55,7 @@ model = AutoModel.from_pretrained(args.model_name).cuda()
 model.eval()
 datafull = datasets.load_from_disk(args.dataset_path)
 datafull = datafull['train']
-batchify_fn = DataCollatorWithPadding(tokenizer=tokenizer,max_length=args.max_seq_length)
+batchify_fn = MyDataCollatorWithPadding(tokenizer=tokenizer,max_length=args.max_seq_length)
 data_loader = DataLoader(
     datafull, shuffle=False, collate_fn=batchify_fn, batch_size=args.batch_size
 )
@@ -68,11 +71,14 @@ with torch.no_grad():
         embeddings = model(input_ids=batch['input_ids'].cuda(),
                            attention_mask=batch['attention_mask'].cuda(),
                            output_hidden_states=True, return_dict=True).pooler_output
-        samples.extend(batch['labels'].numpy())
+        samples.extend(batch['text'])
         embs.extend(embeddings.cpu().numpy())
         progress_bar.update(1)
-        if len(samples) >= BATCH:
-            np.savez(args.save+args.dataset_path+'_'+str(BATCH_IDX),samples=np.array(samples),embs=np.array(embs))
+        if len(samples) >= BATCH or step == len(data_loader)-1:
+            np.savez(args.save+args.dataset_path+'_'+str(BATCH_IDX),embs=np.array(embs),samples=np.array(samples))
+            # with open(args.save+args.dataset_path+'_'+str(BATCH_IDX)+'.txt', 'w',encoding='utf-8') as f:
+            #     for line in samples:
+            #         f.write(line+ ' \n')
             samples = []
             embs = []
             BATCH_IDX += 1
