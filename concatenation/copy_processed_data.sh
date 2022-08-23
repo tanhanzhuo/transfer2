394e7c026b67a3e8788816fe13c1424855478322
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-08-23 16:02:45 +0800

    Update copy_processed_data.sh

--- concatenation/copy_processed_data.sh
+++ concatenation/copy_processed_data.sh
@@ -1,6 +1,6 @@
 for task in stance hate sem-18 sem-17 imp-hate sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm
 do
-  for method in modelT100N100R_fileT100N100R_num10 modelT100N100R_fileT100N1000R_num10 fulldata_simcse
+  for method in modelT100N100R_fileT100N100R_num10 modelT100N100R_fileT100N1000R_num10 fulldata_simcse fulldata_bt_hashremove
   do
     scp -r thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/hash_${method}_top_* transfer2/finetune/data/${task}/
   done
