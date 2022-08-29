b4d68b8d3f66c425a80355b7d4a5ad785fb863e4
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-08-29 16:44:42 +0800

    Update copy_processed_data.sh

--- concatenation/copy_processed_data.sh
+++ concatenation/copy_processed_data.sh
@@ -1,12 +1,12 @@
 for task in stance hate sem-18 sem-17 imp-hate sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm sem18-task1-affect sem21-task7-humor
 do
-  rm -rf /home/thz/transfer2/finetune/data/${task}/emojiorhash_*
-  rm -rf /home/thz/transfer2/finetune/data/${task}/emojihash_*
-  rm -rf /home/thz/transfer2/finetune/data/${task}/emoji_*
-  rm -rf /home/thz/transfer2/finetune/data/${task}/model*
-  rm -rf /home/thz/transfer2/finetune/data/${task}/feature_*
-  rm -rf /home/thz/transfer2/finetune/data/${task}/hash_10*
-  #scp -r thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/hash_* /home/thz/transfer2/finetune/data/${task}/
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/emojiorhash_*
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/emojihash_*
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/emoji_*
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/model*
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/feature_*
+#  rm -rf /home/thz/transfer2/finetune/data/${task}/hash_10*
+  scp -r thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/hash_* /home/thz/transfer2/finetune/data/${task}/
   #for method in modelT100N100R_fileT100N100R_num10 fulldata_simcse fulldata_bt_hashremove
   #do
     #scp -r thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/hash_${method}_top_* /home/thz/transfer2/finetune/data/${task}/
