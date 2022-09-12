40f30f3ee0d6f0c3fdb60408c6c55caa75e64f58
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-09-12 11:02:20 +0800

    Update hash_word_sim.py

--- hashsim/hash_word_sim.py
+++ hashsim/hash_word_sim.py
@@ -1,13 +1,16 @@
 import json
+from tqdm import tqdm
 with open('../contrastive_full/thre100_index.json', 'r', encoding='utf-8') as f:
     CONVERT = json.load(f)
 hashtags = CONVERT.values()
 
 file = open('hash_word_sim.txt', 'a', encoding='utf-8')
-for hashone in hashtags:
+for hashone in tqdm(hashtags):
     hash_save = ''+hashone
     for hashtwo in hashtags:
-        if hashone in hashtwo or hashtwo in hashone:
+        if hashone in hashtwo:
             hash_save += '\t'+hashtwo
+        elif hashtwo in hashone and len(hashtwo)>2:
+            hash_save += '\t' + hashtwo
     hash_save+=' \n'
     file.write(hash_save)
