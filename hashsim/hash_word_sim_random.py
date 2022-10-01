681592b29bf651e93048c7c7c1606643af31605a
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-10-01 10:49:10 +0800

    Create hash_word_sim_random.py

---
+++ hashsim/hash_word_sim_random.py
@@ -0,0 +1,15 @@
+import json
+from tqdm import tqdm
+import random
+with open('../contrastive_full/thre100_index.json', 'r', encoding='utf-8') as f:
+    CONVERT = json.load(f)
+hashtags = CONVERT.values()
+TOP = 5
+file = open('hash_word_sim_random.txt', 'a', encoding='utf-8')
+for hashone in tqdm(hashtags):
+    val = random.sample(hashtags, TOP)
+    tmp_merge = '' + hashone
+    for hash_tmp in val:
+        tmp_merge = tmp_merge + '\t' + hash_tmp
+    tmp_merge += ' \n'
+    file.write(tmp_merge)
