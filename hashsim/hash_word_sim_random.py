59476861ccf2655bcfdf14b708af4a0ae9ed36e0
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-10-01 10:50:42 +0800

    Update hash_word_sim_random.py

--- hashsim/hash_word_sim_random.py
+++ hashsim/hash_word_sim_random.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 import random
 with open('../contrastive_full/thre100_index.json', 'r', encoding='utf-8') as f:
     CONVERT = json.load(f)
-hashtags = CONVERT.values()
+hashtags = list(CONVERT.values())
 TOP = 5
 file = open('hash_word_sim_random.txt', 'a', encoding='utf-8')
 for hashone in tqdm(hashtags):
