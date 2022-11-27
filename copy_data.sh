1bdc2a6aae8cd9d5070d6c242c4dc37f36f84f00
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-11-27 21:09:32 +0800

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -1,4 +1,4 @@
-for task in stance hate sem-18 sem-17 imp-hate sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm sem18-task1-affect sem21-task7-humor
+for task in stance sem-18 sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm sem18-task1-affect sem21-task7-humor
 do
   for sp in train dev test
     do
