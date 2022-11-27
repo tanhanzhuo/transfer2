af52fdb53316fabce187bfc91f71508d8081ea51
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-11-27 21:07:18 +0800

    Update copy_data.sh

--- copy_data.sh
+++ copy_data.sh
@@ -2,6 +2,6 @@ for task in stance hate sem-18 sem-17 imp-hate sem19-task5-hate sem19-task6-offe
 do
   for sp in train dev test
     do
-    scp thz@10.21.4.51:/home/thz/transfer2/finetune/data/${task}/${sp}_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json /home/thz/transfer2/finetune/data/${task}/
+    scp -r thz@10.21.4.51:/home/thz/transfer2/finetune/data/${task}/${sp}_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json ./finetune/data/${task}/
     done
 done
