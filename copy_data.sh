23bab35bc366157b6eda3a9910cb243a7ea41075
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2022-11-27 21:04:47 +0800

    Create copy_data.sh

---
+++ copy_data.sh
@@ -0,0 +1,7 @@
+for task in stance hate sem-18 sem-17 imp-hate sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm sem18-task1-affect sem21-task7-humor
+do
+  for sp in train dev test
+    do
+    scp thz@10.21.4.51:/home/thz/transfer2/finetune/data/${task}/${sp}_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json /home/thz/transfer2/finetune/data/${task}/
+    done
+done
