0067acf84fc27004b3ae2b856ba0e8e3b085c4cb
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-01-04 17:41:34 +0800

    reformulate 111

--- tweeteval/reformulate_clean_data.py
+++ tweeteval/reformulate_clean_data.py
@@ -1,13 +1,18 @@
 import json
 import random
 import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset',default='eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm,stance',type=str)
+parser.add_argument('--split',default='111',type=str)
+args = parser.parse_args()
 
 for epoch in range(10):
-    for task in ['eval-irony_clean','eval-offensive_clean','eval-emotion_clean','eval-stance_clean']:
-        if not os.path.isdir('../finetune/data/' + task + '_evensplit'+str(epoch)):
-            os.mkdir('../finetune/data/' + task + '_evensplit'+str(epoch))
+    for task in args.dataset.split(','):
+        if not os.path.isdir('../finetune/data/' + task + '_clean_'+args.split+'_'+str(epoch)):
+            os.mkdir('../finetune/data/' + task + '_clean_'+args.split+'_'+str(epoch))
         data_tmp = []
-        with open('../finetune/data/' + task + '/all.json', 'r', encoding='utf-8') as f:
+        with open('../finetune/data/' + task + '_clean/all.json', 'r', encoding='utf-8') as f:
             lines = f.readlines()
             for line in lines:
                 one = json.loads(line)
@@ -15,21 +20,23 @@ for epoch in range(10):
 
             random.shuffle(data_tmp)
             total_num = len(data_tmp)
-            split_num = int(total_num/3)
+            train_num = int(args.split[0])
+            dev_num = int(args.split[1])
+            test_num = int(args.split[2])
+            split_num = int(total_num/(train_num+dev_num+test_num))
 
-
-            with open('../finetune/data/' + task + '_evensplit'+str(epoch)+'/' + 'train.json', 'w', encoding='utf-8') as f:
-                for idx in range(0,split_num):
+            with open('../finetune/data/' + task + '_clean_'+args.split+'_'+str(epoch)+'/' + 'train.json', 'w', encoding='utf-8') as f:
+                for idx in range(0,split_num*train_num):
                     one = data_tmp[idx]
                     tmp = json.dumps(one, ensure_ascii=False)
                     f.write(tmp + '\n')
-            with open('../finetune/data/' + task + '_evensplit'+str(epoch)+'/' + 'dev.json', 'w', encoding='utf-8') as f:
-                for idx in range(split_num, split_num*2):
+            with open('../finetune/data/' + task + '_clean_'+args.split+'_'+str(epoch)+'/' + 'dev.json', 'w', encoding='utf-8') as f:
+                for idx in range(split_num*train_num, split_num*(train_num+dev_num)):
                     one = data_tmp[idx]
                     tmp = json.dumps(one, ensure_ascii=False)
                     f.write(tmp + '\n')
-            with open('../finetune/data/' + task + '_evensplit'+str(epoch)+'/' + 'test.json', 'w', encoding='utf-8') as f:
-                for idx in range(split_num*2, total_num):
+            with open('../finetune/data/'  + task + '_clean_'+args.split+'_'+str(epoch)+'/' + 'test.json', 'w', encoding='utf-8') as f:
+                for idx in range(split_num*(train_num+dev_num), total_num):
                     one = data_tmp[idx]
                     tmp = json.dumps(one, ensure_ascii=False)
                     f.write(tmp + '\n')
