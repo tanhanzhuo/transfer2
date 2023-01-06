2c21ee0c4aae2b498f8556fb1c0c98b1b6f0bf78
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-01-06 22:17:54 +0800

    Update retrieve_data_using_previous_exactmatch.py

--- tweeteval/retrieve_data_using_previous_exactmatch.py
+++ tweeteval/retrieve_data_using_previous_exactmatch.py
@@ -11,7 +11,7 @@ import copy
 parser = argparse.ArgumentParser()
 parser.add_argument('--tasks',default='eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm,stance',type=str)
 parser.add_argument('--sp',default='_clean_111_',type=str)
-parser.add_argument('--method',default='_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst',type=str)
+parser.add_argument('--method',default='_fulldata_simcse_top20_textfirst',type=str)#'_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst'
 args = parser.parse_args()
 
 for task in args.tasks.split(','):
@@ -38,8 +38,14 @@ for task in args.tasks.split(','):
                       encoding='utf-8') as f:
                 for idx in range(len(data_sem)):
                     one_text = data_sem[idx]['text'].strip()
+                    match = 0
                     for idx2 in range(len(data_source_text)):
                         two_text = data_source_text[idx2]
                         if one_text == two_text:
                             tmp = json.dumps(data_source[idx2], ensure_ascii=False)
                             f.write(tmp + '\n')
+                            match = 1
+                            break
+                    if match == 0:
+                        print(one_text)
+
