8d9b2c2086412ad103dcc24b5be8b1da0c3e7a0f
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-01-06 22:04:55 +0800

    Update copy_processed_data.sh

--- tweeteval/copy_processed_data.sh
+++ tweeteval/copy_processed_data.sh
@@ -1,7 +1,7 @@
 for task in eval-stance eval-emotion eval-irony eval-offensive eval-hate sem21-task7-humor sem22-task6-sarcasm stance
 do
 #  rm -rf /home/thz/transfer2/finetune/data/${task}/hash_10*
-  scp thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/\{train_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,train_fulldata_simcse_top20_textfirst.json,dev_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,dev_fulldata_simcse_top20_textfirst.json,test_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,test_fulldata_simcse_top20_textfirst.json\} /home/user/transfer2/finetune/data/${task}/
+  scp thz@10.21.4.61:/home/thz/transfer2/finetune/data/${task}/\{train_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,train_fulldata_simcse_top20_textfirst.json,dev_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,dev_fulldata_simcse_top20_textfirst.json,test_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst.json,test_fulldata_simcse_top20_textfirst.json\} /home/thz/transfer2/finetune/data/${task}/
 
   #for method in modelT100N100R_fileT100N100R_num10 fulldata_simcse fulldata_bt_hashremove
   #do
