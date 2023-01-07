a6b608506485436709faf96932410cb7ff519d23
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-01-07 23:15:58 +0800

    match one

--- tweeteval/read_results.py
+++ tweeteval/read_results.py
@@ -17,14 +17,25 @@ def read_txt(file):
         elif len(line)>5:
             num = float(line.split(',')[0])
             results1[cur_task].append(num)
+    for task in results1.keys():
+        scores = results1[task]
+        new_scores = []
+        me = np.median(scores)
+        for score in scores:
+            if score < me*0.9:
+                continue
+            else:
+                new_scores.append(score)
+        results1[task] = new_scores
+
     return  results1
 
-re_ori = read_txt('results_lr1e5_ori_full_early.txt')
-re_roberta = read_txt('results_lr1e5_ori_full_early_roberta.txt')
+re_ori = read_txt('results_111_lrtune_full_lr1e5.txt')
+re_roberta = read_txt('results_111_lrtune_full_lr1e5_extend_type1.txt')
 # re_time = read_txt('results_ori_full_early_weight_time.txt')
 
 
-for task in re_ori.keys():
+for task in re_roberta.keys():
     st, val_ro = ttest_ind(re_ori[task], re_roberta[task])
     # st, val_ti = ttest_ind(re_ori[task], re_time[task])
     # print('task:{}, bertweet:{:.5f},std:{:.5f}, roberta:{:.5f},std:{:.5f}, time:{:.5f},std:{:.5f}'\
---
+++ tweeteval/retrieve_data_using_previous_exactmatch_one.py
@@ -0,0 +1,51 @@
+import json
+import datasets
+import argparse
+import torch
+import numpy as np
+from tqdm import tqdm,trange
+import emoji
+from scipy.spatial.distance import pdist, squareform
+import os
+import copy
+parser = argparse.ArgumentParser()
+parser.add_argument('--tasks',default='eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm,stance',type=str)
+parser.add_argument('--target',default='_clean',type=str)
+parser.add_argument('--method',default='_fulldata_simcse_top20_textfirst',type=str)#'_modelT100N100M_fileT100N100S_num10_cluster_top20_textfirst'
+args = parser.parse_args()
+
+for task in args.tasks.split(','):
+    print(task)
+    data_source = []
+    data_source_text = []
+    for sp in ['train', 'dev', 'test']:
+        with open('../finetune/data/' + task + '/' + sp + args.method + '.json', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                one = json.loads(line)
+                data_source.append(one)
+                data_source_text.append(one['text'].split(' \n ')[-2].strip())
+
+    for sp in ['train', 'dev', 'test']:
+        data_sem = []
+        with open('../finetune/data/' + task + args.target + '/' + sp + '.json', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                one = json.loads(line)
+                data_sem.append(one['text'].strip())
+
+        with open('../finetune/data/' + task + args.target + '/' + sp + args.method + '.json', 'w',
+                  encoding='utf-8') as f:
+            for idx in range(len(data_sem)):
+                one_text = data_sem[idx]
+                match = 0
+                for idx2 in range(len(data_source_text)):
+                    two_text = data_source_text[idx2]
+                    if one_text == two_text:
+                        tmp = json.dumps(data_source[idx2], ensure_ascii=False)
+                        f.write(tmp + '\n')
+                        match = 1
+                        break
+                if match == 0:
+                    print(one_text)
+
