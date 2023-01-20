8a07035eafb1a2efdfefc07e92f2ee0aeb8cfe3e
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-01-20 21:18:59 +0800

    Create read_hash_time.py

---
+++ tweeteval/read_hash_time.py
@@ -0,0 +1,25 @@
+import json
+import datasets
+import numpy as np
+from sklearn import metrics
+import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", default='../contrastive_full/feature_modelT100N100M_fileT100N100S_num10_cluster', type=str, required=False)
+parser.add_argument("--N", default=10000, type=int, required=False)
+
+args = parser.parse_args()
+
+hash_embs = []
+hash_tags = []
+for idx in range(4):
+    tmp = np.load(args.file+'_'+str(idx)+'.npz',allow_pickle=True)
+    hash_embs.extend(tmp['center_embs'])
+    tmp.close()
+
+time1 = time.time()
+for idx in range(N):
+    d=metrics.pairwise.cosine_similarity([hash_embs[idx]],hash_embs)
+time2 = time.time()
+print((time2-time1)/args.N)
