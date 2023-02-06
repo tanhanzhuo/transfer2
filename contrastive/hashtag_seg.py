6dcb1cd617e09126edc239bbfa52f28f113326ee
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-02-06 11:49:37 +0800

    Update hashtag_seg.py

--- contrastive/hashtag_seg.py
+++ contrastive/hashtag_seg.py
@@ -4,13 +4,20 @@ import torch
 from tqdm import tqdm, trange
 import json
 import re
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--file',default='tweet_hash_clean_group.txt',type=str)
+parser.add_argument('--num',default=20,type=int)
+
+args = parser.parse_args()
+
 data =[]
 num = []
 f=open('./hash_his.json','r',encoding='utf-8')
 hash_dic = json.load(f)
 f.close()
 for hash_one in list(hash_dic.keys()):
-    if hash_dic[hash_one] < 100:
+    if hash_dic[hash_one] < args.num:
         hash_dic.pop(hash_one)
 
 from hashformers import TransformerWordSegmenter as WordSegmenter
@@ -18,7 +25,7 @@ with torch.no_grad():
     data_seg = []
     ws = WordSegmenter( segmenter_model_name_or_path="gpt2")
 
-    with open('hash_seg.txt', 'a') as f:
+    with open('hash_seg'+str(args.num)+'.txt', 'a') as f:
         for hash_one in tqdm(hash_dic.keys()):
             segmentations = ws.segment([hash_one])
             f.write(hash_one + '\t' + segmentations[0] + '\t' + str(hash_dic[hash_one]) + '\n')
