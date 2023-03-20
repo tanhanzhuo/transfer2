e92feb628400242307376588479efb65b49a113e
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-03-20 23:18:22 +0800

    Update bertopic_test.py

--- bertopic/bertopic_test.py
+++ bertopic/bertopic_test.py
@@ -4,7 +4,7 @@ from sklearn.datasets import fetch_20newsgroups
 
 docs = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))['data']
 
-embedding_model = pipeline("feature-extraction", model="princeton-nlp/sup-simcse-roberta-base")
-topic_model = BERTopic(embedding_model=embedding_model)
+embedding_model = pipeline("feature-extraction", model="princeton-nlp/sup-simcse-roberta-base", device=0)
+topic_model = BERTopic(embedding_model=embedding_model, verbose=True)
 topics, probs = topic_model.fit_transform(docs)
 print(topic_model.get_topic_info())
