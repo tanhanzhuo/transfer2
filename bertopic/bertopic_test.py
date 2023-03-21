5feb2c5dbd508367a20366435898da59dac2ea3f
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-03-21 10:01:53 +0800

    Update bertopic_test.py

--- bertopic/bertopic_test.py
+++ bertopic/bertopic_test.py
@@ -5,7 +5,8 @@ from sentence_transformers import SentenceTransformer
 
 docs = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))['data']
 
-embedding_model = SentenceTransformer("all-mpnet-base-v2")
+# embedding_model = SentenceTransformer("all-mpnet-base-v2")
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 # embedding_model = pipeline("feature-extraction", model="princeton-nlp/sup-simcse-roberta-base", device=0)
 topic_model = BERTopic(embedding_model=embedding_model, verbose=True)
 topics, probs = topic_model.fit_transform(docs)
