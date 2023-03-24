0e83ed9d76b908c9a0335ed477902960d70f991a
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-03-24 10:23:02 +0800

    Create create_data_hashtag_subgroup_noise.py

---
+++ pretrain/hashtag/create_data_hashtag_subgroup_noise.py
@@ -0,0 +1,103 @@
+import argparse
+import json
+import random
+from tqdm import tqdm,trange
+import numpy as np
+import re
+import string
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file',default='../../bertopic/tweet_hash_clean_group_subgroup_100_0.json',type=str)
+parser.add_argument('--num',default=100,type=int)
+parser.add_argument('--name',default='tweet_hash_clean_group_raw',type=str)
+parser.add_argument('--ran1', default=0.333, type=float)
+parser.add_argument('--ran2', default=0.667, type=float)
+parser.add_argument('--max_len', default=512, type=int)
+parser.add_argument('--rep', default=5, type=int)
+parser.add_argument('--balance', default=0, type=int)
+parser.add_argument('--root', default='../../', type=str)
+parser.add_argument('--sep', default=0, type=int)
+
+args = parser.parse_args()
+
+with open(args.root+'/contrastive/hash_his.json', 'r', encoding='utf-8') as f:
+    hash_dic = json.load(f)
+# for hash_one in list(hash_dic.keys()):
+#     if hash_dic[hash_one] < args.num:
+#         hash_dic.pop(hash_one)
+hash_thre_list = list(hash_dic.keys())
+random.shuffle(hash_thre_list)
+
+with open(args.root+'/contrastive/hash_seg10.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+hash_seg = {}
+for line in lines:
+    line = line.strip()
+    hash_seg[line.split('\t')[0]] = line.split('\t')[1]
+
+HASH = re.compile(r"#\S+")
+def process(line):
+    hash_tmp = HASH.findall(line)
+    hash_tmp_clean = []
+    for hash_one in hash_tmp:
+        # hash_one = hash_one.lower()
+        if len(hash_one) > 30:
+            continue
+        if hash_one[1].isalpha():
+            if hash_one[-1] == '…':
+                continue
+            if len(hash_one) > 3 and hash_one[-3:] == '...':
+                continue
+            if hash_one[-1] in string.punctuation:
+                hash_one = hash_one[:-1]
+            hash_clean = re.findall('[a-zA-Z0-9]*', hash_one)
+            hash_clean = '#' + ''.join(hash_clean)
+            if hash_one == hash_clean:
+                hash_tmp_clean.append(hash_one)
+
+    return hash_tmp_clean
+
+hash_data = []
+with open(args.file, 'r', encoding='utf-8') as f:
+    for line in tqdm(f):
+        tmp = json.loads(line)
+        tmp.pop('hashtag')
+        hash_data.append(tmp)
+
+for hash_idx in trange(len(hash_data)):
+    if args.balance == 0:
+        epoch = args.rep
+    else:
+        epoch = min(args.rep, int( args.balance*args.rep*1.0/ sum([len(i) for i in hash_data[hash_idx].values()]) ) )
+    for tmp in range(epoch):
+        hash_data_hash = hash_data[hash_idx]
+        for key1 in hash_data_hash.keys():
+            hash_data_one = hash_data_hash[key1]
+            random.shuffle(hash_data_one)
+            hash_data_one_noise = []
+            for data_tmp in hash_data_one:
+                data_tmp = data_tmp.strip()
+                hash_tmp = process(data_tmp)
+                for hash_two in hash_tmp:
+                    ran1 = np.random.random()
+                    if ran1 < args.ran1:
+                        data_tmp = data_tmp.replace(hash_two, '')
+                    elif ran1 < args.ran2:
+                        tmp2 = hash_seg.get(hash_two.lower())
+                        if tmp2 is not None:
+                            data_tmp = data_tmp.replace(hash_two, tmp2)
+                        else:
+                            data_tmp = data_tmp.replace(hash_two, hash_two[1:])
+
+                hash_data_one_noise.append(data_tmp)
+        with open(args.name + '_' + str(args.num) + '.txt', 'a', encoding='utf-8') as f:
+            hash_data_group = ''
+            for idx in range(len(hash_data_one_noise)):
+                if args.sep == 0:
+                    hash_data_group += hash_data_one_noise[idx] + ' '
+                else:
+                    hash_data_group += hash_data_one_noise[idx] + ' </s> '
+                if len(hash_data_group) > args.max_len*0.95:
+                    f.write(hash_data_group+'\n')
+                    hash_data_group = ''
+
