37bcd0f3401df9fc3a74fa90069196ad793bd9f8
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-03-24 12:20:45 +0800

    Update tweet_group_eachhash_clean_func.py

--- bertopic/tweet_group_eachhash_clean_func.py
+++ bertopic/tweet_group_eachhash_clean_func.py
@@ -100,6 +100,7 @@ def group_one(hash_data_one, hash_one):
     # print(hash_data_two)
     topics, probs = topic_model.fit_transform(hash_data_two)
     num_topic = max(topics) + 2
+    print(num_topic, len(topic_model.topic_embeddings_))
     # hash_data_one_group = {'hashtag': hash_one, 'text':[[]]*num_topic, 'emb':[]}
     # for idx in range(len(hash_data_one)):
     #     if topics[idx] + 1 in hash_data_one_group.keys():
@@ -114,7 +115,6 @@ def group_one(hash_data_one, hash_one):
         hash_data_one_group['text'][topics[idx] + 1].append(hash_data_one[idx])
     for idx in range(num_topic):
         hash_data_one_group['emb'].append(topic_model.topic_embeddings_[idx])
-    print(num_topic, len(topic_model.topic_embeddings_))
     return hash_data_one_group
 
     # del embedding_model, topic_model
