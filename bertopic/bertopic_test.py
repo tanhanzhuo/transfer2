c045f106a1c746a45c1a95afb0efc91fa8b782f2
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-03-25 11:45:57 +0800

    Update bertopic_test.py

--- bertopic/bertopic_test.py
+++ bertopic/bertopic_test.py
@@ -2,12 +2,16 @@ from bertopic import BERTopic
 from transformers.pipelines import pipeline
 from sklearn.datasets import fetch_20newsgroups
 from sentence_transformers import SentenceTransformer
+from bertopic.dimensionality import BaseDimensionalityReduction
 
 docs = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))['data']
 
 # embedding_model = SentenceTransformer("all-mpnet-base-v2")
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 # embedding_model = pipeline("feature-extraction", model="princeton-nlp/sup-simcse-roberta-base", device=0)
-topic_model = BERTopic(embedding_model=embedding_model, verbose=True)
+# topic_model = BERTopic(embedding_model=embedding_model, verbose=True)
+empty_dimensionality_model = BaseDimensionalityReduction()
+topic_model = BERTopic(embedding_model=embedding_model, verbose=False, umap_model=empty_dimensionality_model)
+
 topics, probs = topic_model.fit_transform(docs)
 print(topic_model.get_topic_info())
