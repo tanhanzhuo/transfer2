68e34167e67f4dcddfd3e312dc10078ae977a53b
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-04-17 19:48:51 +0800

    Update process_retrieval_data.py

--- lmbff/process_retrieval_data.py
+++ lmbff/process_retrieval_data.py
@@ -6,14 +6,28 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", default='../finetune/data/', type=str, required=False, help="The output directory where the model predictions and checkpoints will be written.")
 parser.add_argument("--dataset_path", default='../finetune/data/', type=str, required=False, help="dataset name")
 parser.add_argument("--task_name", default='stance,hate,sem-18,sem-17,imp-hate,sem19-task5-hate,sem19-task6-offen,sem22-task6-sarcasm,sem18-task1-affect,sem21-task7-humor', type=str, required=False, help="dataset name")
-parser.add_argument('--method_hash',default='modelT100N100R_fileT100N100R_num10_top20_textfirst',type=str)
+parser.add_argument('--method',default='modelT100N100R_fileT100N100R_num10_top20_textfirst',type=str)
 parser.add_argument('--top',default=1,type=int)
 parser.add_argument('--name',default='',type=str)
 
+def write_json(data, fileName):
+    with open(fileName + '.json', 'w', encoding='utf-8') as f:
+        for one in data:
+            tmp = json.dumps(one, ensure_ascii=False)
+            f.write(tmp+'\n')
 
 if __name__ == "__main__":
     args = parser.parse_args()
     for task in args.task_name.split(','):
-        for sp in ['train','dev','test':]
-            with open(args.dataset_path+task+'/'+sp, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
+        for sp in ['train','dev','test']:
+            data = []
+            with open(args.dataset_path+task+'/'+sp + args.method + '.json', 'r', encoding='utf-8') as f:
+                for line in f:
+                    one = json.loads(line)
+                    text_sp = one['text'].split(' \n ')
+                    for idx in range(len(text_sp)-1):
+                        one['text'+str(idx)] = text_sp[idx]
+                    one['text'] = text_sp[-1]
+                    data.append(one)
+            write_json(data, args.dataset_path+task+'/'+sp + args.method + '_sp.json')
+
