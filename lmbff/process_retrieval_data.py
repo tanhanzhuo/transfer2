b8198a4347b319fa166b9a183cd4ae11e8b8bd11
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-04-18 10:33:29 +0800

    Update process_retrieval_data.py

--- lmbff/process_retrieval_data.py
+++ lmbff/process_retrieval_data.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", default='../finetune/data/', type=str, required=False, help="The output directory where the model predictions and checkpoints will be written.")
 parser.add_argument("--dataset_path", default='../finetune/data/', type=str, required=False, help="dataset name")
 parser.add_argument("--task_name", default='stance,hate,sem-18,sem-17,imp-hate,sem19-task5-hate,sem19-task6-offen,sem22-task6-sarcasm,sem18-task1-affect,sem21-task7-humor', type=str, required=False, help="dataset name")
-parser.add_argument('--method',default='modelT100N100R_fileT100N100R_num10_top20_textfirst',type=str)
+parser.add_argument('--method',default='_modelT100N100R_fileT100N100R_num10_top20_textfirst',type=str)
 parser.add_argument('--top',default=1,type=int)
 parser.add_argument('--name',default='',type=str)
 
