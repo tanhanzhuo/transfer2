fb4bf2aba2695f83c307d03577e5e8761dcf503e
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-04-27 11:41:51 +0800

    num label 50

--- lmbff/find_word_tmp.py
+++ lmbff/find_word_tmp.py
@@ -349,10 +349,10 @@ def do_train(args):
     # load generation model for word generation
     if args.generate_word == 1:
         if 'roberta' in args.model_name_or_path:
-            verbalizer_generator = RobertaVerbalizerGenerator(model=copy.deepcopy(plm), tokenizer=tokenizer, candidate_num=100,
+            verbalizer_generator = RobertaVerbalizerGenerator(model=copy.deepcopy(plm), tokenizer=tokenizer, candidate_num=50,
                                                               label_word_num_per_class=100)
         else:
-            verbalizer_generator = BertweetVerbalizerGenerator(model=copy.deepcopy(plm), tokenizer=tokenizer, candidate_num=100,
+            verbalizer_generator = BertweetVerbalizerGenerator(model=copy.deepcopy(plm), tokenizer=tokenizer, candidate_num=50,
                                                                label_word_num_per_class=100)
         dataloader = PromptDataLoader(dataset['train'], template, tokenizer=tokenizer,
                                       tokenizer_wrapper_class=MLMTokenizerWrapper,
