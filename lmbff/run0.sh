3429b1724d2bf5a734a7268ff26f60b665de41e0
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-10 11:23:33 +0800

    sarcasm

--- lmbff/run0.sh
+++ lmbff/run0.sh
@@ -35,7 +35,11 @@
 #  CUDA_VISIBLE_DEVICES=1 python find_tmp_auto.py --train ../finetune/data/${TASK}/train_fuldata_bt_hashseg_top20_textfirst.tsv --dev ../finetune/data/${TASK}/dev_fuldata_bt_hashseg_top20_textfirst.tsv --template '<s> [T] [T] [T] {sentence_A} [T] [T] [T] {sentence_B} . It was [P] . </s>' --num-cand 100 --accumulation-steps 30 --bsz 24 --eval-size 48 --iters 100 --model-name vinai/bertweet-base --log_name bt_retri.log --max_seq_length 400 --label-map 3
 #done
 
-for TASK in eval-offensive
+#for TASK in eval-offensive
+#do
+#  CUDA_VISIBLE_DEVICES=5 python find_tmp_auto_new.py --train ../finetune/data/${TASK}/train_fuldata_bt_hashseg_top20_textfirst.tsv --dev ../finetune/data/${TASK}/dev_fuldata_bt_hashseg_top20_textfirst.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 100 --accumulation-steps 30 --bsz 24 --eval-size 48 --iters 100 --model-name vinai/bertweet-base --log_name bt_retri_ori_03_1.log --max_seq_length 400 --filter
+#done
+for TASK in eval-stance eval-emotion eval-irony eval-offensive eval-hate sem21-task7-humor sem22-task6-sarcasm
 do
-  CUDA_VISIBLE_DEVICES=5 python find_tmp_auto_new.py --train ../finetune/data/${TASK}/train_fuldata_bt_hashseg_top20_textfirst.tsv --dev ../finetune/data/${TASK}/dev_fuldata_bt_hashseg_top20_textfirst.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 100 --accumulation-steps 30 --bsz 24 --eval-size 48 --iters 100 --model-name vinai/bertweet-base --log_name bt_retri_ori_03_1.log --max_seq_length 400 --filter
+  CUDA_VISIBLE_DEVICES=0 python find_tmp_auto_new.py --train ../finetune/data/${TASK}/train_fulldata_simcse_top20_textfirst.tsv --dev ../finetune/data/${TASK}/dev_fulldata_simcse_top20_textfirst.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 100 --accumulation-steps 30 --bsz 20 --eval-size 48 --iters 100 --model-name vinai/bertweet-base --log_name template_bt_retrisimcse_labelori_tmp03.log --max_seq_length 400 --filter --seed 0
 done
