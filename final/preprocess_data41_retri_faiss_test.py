7892498a2965b4076eef33bc10034cbcb66f3a6b
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-12 15:41:58 +0800

    Update preprocess_data41_retri_faiss_test.py

--- final/preprocess_data41_retri_faiss_test.py
+++ final/preprocess_data41_retri_faiss_test.py
@@ -74,7 +74,7 @@ print(hash_embs.shape)
 train_s = time.time()
 # quantizer = faiss.IndexFlatL2(dim)  # def the method of calculating distance (L2 distance, here)
 # cpu_index = faiss.IndexIVFPQ(quantizer, dim, int(len(hash_embs)/100), 8, 8)  # construct the index
-cpu_index = faiss.index_factory(dim, "OPQ8,IVF600000,PQ8")
+cpu_index = faiss.index_factory(dim, "IVF600000,PQ8")
 gpu_index = faiss.index_cpu_to_all_gpus(cpu_index)
 gpu_index.train(hash_embs)                       # train the index on the data
 train_e = time.time()
