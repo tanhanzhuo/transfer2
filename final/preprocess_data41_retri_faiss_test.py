afd1213da162275c95a7edd365f0505ead0dcbe7
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-12 11:13:39 +0800

    Update preprocess_data41_retri_faiss_test.py

--- final/preprocess_data41_retri_faiss_test.py
+++ final/preprocess_data41_retri_faiss_test.py
@@ -70,10 +70,12 @@ print(hash_embs.shape)
 
 
 train_s = time.time()
-quantizer = faiss.IndexFlatIP(dim)  # def the method of calculating distance (L2 distance, here)
-cpu_index = faiss.IndexIVFPQ(quantizer, dim, int(len(hash_embs)/100), 8, 8)  # construct the index
+# quantizer = faiss.IndexFlatIP(dim)  # def the method of calculating distance (L2 distance, here)
+# cpu_index = faiss.IndexIVFPQ(quantizer, dim, int(len(hash_embs)/100), 8, 8)  # construct the index
+cpu_index = faiss.index_factory(dim, "IVF262144_HNSW32,Flat")
 gpu_index = faiss.index_cpu_to_all_gpus(cpu_index)
 gpu_index.train(hash_embs)                       # train the index on the data
+gpu_index.add(hash_embs)
 train_e = time.time()
 print('ivfpq build time: {}'.format(train_e - train_s))
 
