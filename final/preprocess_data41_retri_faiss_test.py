7b9756e2be9455228c0cf0929063d0eda966ef7c
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-12 22:44:09 +0800

    Update preprocess_data41_retri_faiss_test.py

--- final/preprocess_data41_retri_faiss_test.py
+++ final/preprocess_data41_retri_faiss_test.py
@@ -51,49 +51,15 @@ dim = len(hash_embs[0])
 print('input dimension:')
 print(hash_embs.shape)
 
-# time1 = time.time()
-# cpu_index = faiss.IndexFlatIP(dim)  # 构建索引index
-# gpu_index = cpu_index#faiss.index_cpu_to_all_gpus(cpu_index)
-# gpu_index.add(hash_embs)
-# time2 = time.time()
-# print('direct build time:{}'.format(time2-time1))
-# k = 100  # 返回结果个数
-# query = hash_embs[:args.num]  # 查询本身
-#
-# time1 = time.time()
-# dis, ind = gpu_index.search(query, k)
-# time2 = time.time()
-# print('direct search time:{}'.format(time2-time1))
-# print('shape of dis and idx')
-# print(dis.shape)
-# print(ind.shape)
-#
-# del cpu_index, gpu_index
-
-
-train_s = time.time()
-# quantizer = faiss.IndexFlatL2(dim)  # def the method of calculating distance (L2 distance, here)
-# cpu_index = faiss.IndexIVFPQ(quantizer, dim, int(len(hash_embs)/100), 8, 8)  # construct the index
-cpu_index = faiss.index_factory(dim, "IVF600000,PQ8")
-gpu_index = faiss.index_cpu_to_all_gpus(cpu_index)
-gpu_index.train(hash_embs)                       # train the index on the data
-train_e = time.time()
-print('index build time: {}'.format(train_e - train_s))
-
-cpu_index = faiss.index_gpu_to_cpu(gpu_index)
-faiss.write_index(cpu_index, 'tweet_hash_clean_seg_one20_index.index')
-
 time1 = time.time()
+cpu_index = faiss.IndexFlatIP(dim)  # 构建索引index
+gpu_index = cpu_index#faiss.index_cpu_to_all_gpus(cpu_index)
 gpu_index.add(hash_embs)
 time2 = time.time()
-print('index add time:{}'.format(time2-time1))
-cpu_index = faiss.index_gpu_to_cpu(gpu_index)
-faiss.write_index(cpu_index, 'tweet_hash_clean_seg_one20_index_add.index')
-
-
+print('direct build time:{}'.format(time2-time1))
 k = 100  # 返回结果个数
 query = hash_embs[:args.num]  # 查询本身
-gpu_index.nprobe = int(len(hash_embs)/1000)
+
 time1 = time.time()
 dis, ind = gpu_index.search(query, k)
 time2 = time.time()
@@ -101,4 +67,38 @@ print('direct search time:{}'.format(time2-time1))
 print('shape of dis and idx')
 print(dis.shape)
 print(ind.shape)
+#
+# del cpu_index, gpu_index
+
+
+# train_s = time.time()
+# # quantizer = faiss.IndexFlatL2(dim)  # def the method of calculating distance (L2 distance, here)
+# # cpu_index = faiss.IndexIVFPQ(quantizer, dim, int(len(hash_embs)/100), 8, 8)  # construct the index
+# cpu_index = faiss.index_factory(dim, "IVF600000,PQ8")
+# gpu_index = faiss.index_cpu_to_all_gpus(cpu_index)
+# gpu_index.train(hash_embs)                       # train the index on the data
+# train_e = time.time()
+# print('index build time: {}'.format(train_e - train_s))
+#
+# cpu_index = faiss.index_gpu_to_cpu(gpu_index)
+# faiss.write_index(cpu_index, 'tweet_hash_clean_seg_one20_index.index')
+#
+# time1 = time.time()
+# gpu_index.add(hash_embs)
+# time2 = time.time()
+# print('index add time:{}'.format(time2-time1))
+# cpu_index = faiss.index_gpu_to_cpu(gpu_index)
+# faiss.write_index(cpu_index, 'tweet_hash_clean_seg_one20_index_add.index')
+#
+#
+# k = 100  # 返回结果个数
+# query = hash_embs[:args.num]  # 查询本身
+# gpu_index.nprobe = int(len(hash_embs)/1000)
+# time1 = time.time()
+# dis, ind = gpu_index.search(query, k)
+# time2 = time.time()
+# print('direct search time:{}'.format(time2-time1))
+# print('shape of dis and idx')
+# print(dis.shape)
+# print(ind.shape)
 
