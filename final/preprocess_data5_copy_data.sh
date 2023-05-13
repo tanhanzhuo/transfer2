51a0748f7c8b0193289817b1076f37f298944dc6
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-13 15:58:22 +0800

    raname

---
+++ final/preprocess_data5_copy_data.sh
@@ -0,0 +1,8 @@
+#for task in stance sem-18 sem19-task5-hate sem19-task6-offen sem22-task6-sarcasm sem18-task1-affect sem21-task7-humor
+for task in eval-stance eval-emotion eval-irony eval-offensive eval-hate sem21-task7-humor sem22-task6-sarcasm
+do
+  for sp in train dev test
+    do
+    scp -r thz@10.21.4.51:/home/thz/transfer2/finetune/data/${task}/${sp}_seg_500_one20_top100_sp* ./finetune/data/${task}/
+    done
+done
