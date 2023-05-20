d546e9abf408e800721a04f4e443a285ecba53c7
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-20 15:51:35 +0800

    seed 10

--- final/find11_tmp_auto2.sh
+++ final/find11_tmp_auto2.sh
@@ -1,4 +1,4 @@
 for TASK in eval-stance eval-emotion eval-irony eval-offensive eval-hate sem21-task7-humor sem22-task6-sarcasm
 do
-  CUDA_VISIBLE_DEVICES=3 python find10_tmp_auto_f1.py --train ../finetune/data/${TASK}/train_seg_500_one20_top100_sp.tsv --dev ../finetune/data/${TASK}/dev_seg_500_one20_top100_sp.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 50 --accumulation-steps 50 --bsz 16 --eval-size 48 --iters 30 --model-name vinai/bertweet-base --log_name bt_retrione_label_03_seed1.log --max_seq_length 400 --filter --seed 1
+  CUDA_VISIBLE_DEVICES=6 python find10_tmp_auto_f1.py --train ../finetune/data/${TASK}/train_seg_500_one20_top100_sp.tsv --dev ../finetune/data/${TASK}/dev_seg_500_one20_top100_sp.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 50 --accumulation-steps 50 --bsz 16 --eval-size 48 --iters 30 --model-name vinai/bertweet-base --log_name bt_retrione_label_03_seed10.log --max_seq_length 400 --filter --seed 10
 done
