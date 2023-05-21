ae13c1e2a2bbad25e7472f22f4a1707fbbd05547
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-05-21 22:41:17 +0800

    05 tmp

--- final/find11_tmp_auto.sh
+++ final/find11_tmp_auto.sh
@@ -1,4 +1,4 @@
 for TASK in eval-stance eval-emotion eval-irony eval-offensive eval-hate sem21-task7-humor sem22-task6-sarcasm
 do
-  CUDA_VISIBLE_DEVICES=5 python find10_tmp_auto_f1.py --train ../finetune/data/${TASK}/train_seg_500_three20_top100_sp.tsv --dev ../finetune/data/${TASK}/dev_seg_500_three20_top100_sp.tsv --template '<s> {sentence_B} </s> [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 50 --accumulation-steps 50 --bsz 16 --eval-size 48 --iters 30 --model-name vinai/bertweet-base --log_name bt_retrithree_label_03_seed10.log --max_seq_length 400 --filter --seed 10
+  CUDA_VISIBLE_DEVICES=2 python find10_tmp_auto_f1.py --train ../finetune/data/${TASK}/train_seg_500_three20_top100_sp.tsv --dev ../finetune/data/${TASK}/dev_seg_500_three20_top100_sp.tsv --template '<s> {sentence_B} </s> [T] [T] [T] [T] [T] {sentence_A} . It was [P] . </s>' --num-cand 50 --accumulation-steps 50 --bsz 16 --eval-size 48 --iters 30 --model-name vinai/bertweet-base --log_name bt_retrithree_label_05.log --max_seq_length 400 --filter --seed 0
 done
