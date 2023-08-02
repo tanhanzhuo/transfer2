8eddef2534d1e1cdf4ada4c10b65908edb86cee3
tanhanzhuo <tanhanzhuo.baidu@gmail.com>
2023-08-02 14:56:16 +0800

    Update find2_print_trigger.py

--- final/find2_print_trigger.py
+++ final/find2_print_trigger.py
@@ -80,6 +80,11 @@ parser.add_argument(
     type=str,
     required=False,
 )
+def check(model1,model2):
+    for p1, p2 in zip(model1.parameters(), model2.parameters()):
+        if p1.data.ne(p2.data).sum() > 0:
+            return False
+    return True
 
 with torch.no_grad():
     # cos_sim = torch.nn.CosineSimilarity(dim=1).cuda()
